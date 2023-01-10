3400757cc162a41930d815a1aa26757f24d4eb46
Henry Okeke <iamhenryokeke@gmail.com>
2023-01-10 22:54:07 +0100

    added custom fonts and color config for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./public/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        body:['Jost']
+      },colors: {
+        indigo: {
+          950: '#4346E7',
+          1000: '#8E8FE0',
+        },neutral : {
+          950 : '#121212',
+          1000 : '#D9D9D9'
+        }
+      }
+    },
   },
   plugins: [],
 }
